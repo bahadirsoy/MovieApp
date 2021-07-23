7f1231e0edec97c223d00663a6f6b37ab5c5814d
Bahadir Ustabasi <oxikairos@hotmail.com>
2021-07-23 17:36:30 +0300

    mvc added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApp.Controllers
+{
+    public class HomeController: Controller
+    {
+        public IActionResult Index()
+        {
+          return View();
+        }
+        public IActionResult Contact()
+        {
+          return View();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,7 @@ namespace MovieApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +27,8 @@ namespace MovieApp
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            // home/index/3
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
